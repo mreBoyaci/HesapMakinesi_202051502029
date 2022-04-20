fe04ffc107c9edf4dc81d78abf17797867470d4d
asdds <asdds@DESKTOP-4N5JBVQ>
2022-04-20 16:07:20 +0300

    Add project files.

---
+++ HesapMakinesi_202051502029/HesapMakinesi.cs
@@ -0,0 +1,24 @@
+﻿class HesapMakinesi
+{
+    public static double islemYap(double sayi1, double sayi2, string op)
+    {
+        // operatör değerine göre case işlemlerinden biri gerçekleştirilir.
+        switch (op)
+        {
+            case "+": return sayi1 + sayi2;
+            case "-": return sayi1 - sayi2;
+            case "*": return sayi1 * sayi2;
+            case "/":
+                // 2. sayı 0 değilse işlemi yapar.
+                if (sayi2 != 0)
+                {
+                    return sayi1 / sayi2;
+                }
+                else
+                {
+                    return double.NaN;
+                }
+            default: return double.NaN;
+        }
+    }
+}
